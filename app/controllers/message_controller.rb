0c1f3d4380f456a5df6dd15e9561d4a7cf650c1b
Yoko Harada <yokolet@gmail.com>
2017-08-22 16:40:38 +0900

    Adds legacy controller

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,6 @@
+class MessageController < ApplicationController
+  def index
+    time = Time.now
+    render json: {status: 'SUCCESS', message: 'Current time on the server', data: time}, status: :ok
+  end
+end
