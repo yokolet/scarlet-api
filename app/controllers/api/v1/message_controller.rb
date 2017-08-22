9acb64c3d32800c693585abbbad0e1ed3b00662d
Yoko Harada <yokolet@gmail.com>
2017-08-22 22:02:46 +0900

    chores: makes module names succinct

--- app/controllers/api/v1/message_controller.rb
+++ app/controllers/api/v1/message_controller.rb
@@ -1,10 +1,6 @@
-module Api
-  module V1
-    class MessageController < ApplicationController
-      def index
-        time = Time.now
-        render json: {status: 'SUCCESS', message: 'Current time on the server', data: time}, status: :ok
-      end
-    end
+class Api::V1::MessageController < ApplicationController
+  def index
+    time = Time.now
+    render json: {status: 'SUCCESS', message: 'Current time on the server', data: time}, status: :ok
   end
 end
