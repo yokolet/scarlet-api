41941891dd6be994d1fc8651178927fe2596b7b4
Yoko Harada <yokolet@gmail.com>
2017-08-23 14:44:13 +0900

    delete unused

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,13 @@ class ApplicationController < ActionController::API
 
   def create_client
     @@threescale_client ||= ThreeScale::Client.new(service_tokens: true)
-    @@info = {}
   end
 
   def authenticate
     response = create_client.authrep(
       service_token: ENV['THREESCALE_SERVICE_TOKEN'],
       service_id: ENV['THREESCALE_SERVICE_ID'],
-      user_key: params['USER_KEY'],
+      user_key: params['user_key'],
       usage: {'lorem_hits' => 1})
     if response.success?
       return true
